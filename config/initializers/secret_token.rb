1bd8afae961d110fe67b862035493cf86cfe441e
Taylor Hobbs <taylor.hobbs@digibox.com.au>
2013-03-17 13:22:48 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digibox::Application.config.secret_token = 'a4d2fd9588402af24bae6c0e3e1224dfb38aed73e92268067bb359abe5c1130b6be2d22e692085019d56c012bb3f07464a3bfb1b43889c030fbbc1c06188acab'
